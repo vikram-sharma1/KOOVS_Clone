381fdf888eeaede4560a969b64ea120652d49543
PrajwalDhoble <95961767+PrajwalDhoble@users.noreply.github.com>
2021-12-19 20:04:26 +0530

    Add files via upload

---
+++ otp.js
@@ -0,0 +1,18 @@
+
+//   document.querySelector("button").addEventListener("click", next);
+//   var userData = JSON.parse(localStorage.getItem("userDatabase")) || [];
+//   function next() {
+//     var phone=document.querySelector("#pphone").value;
+//     if(phone.length==10){
+//         alert("OTP sent successfully")
+//     }
+//     else{
+//         alert("Please, enter valid mobile number")
+//     }
+  
+//   var userInfo={
+//       phone:phone,
+//   };
+//   userData.push(userInfo);
+//   localStorage.setItem("userDatabase", JSON.stringify(userData))
+// };
